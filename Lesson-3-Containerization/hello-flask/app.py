68a838d41c79defc9faa08e7753a7b9b9e5c6a9c
Ahmed Yassin <ahmed.o.nabri@gmail.com>
2022-10-06 23:16:46 -0500

    Lesson 3, fixing containerization by passing host

--- Lesson-3-Containerization/hello-flask/app.py
+++ Lesson-3-Containerization/hello-flask/app.py
@@ -1,10 +1,18 @@
 # save this as app.py
 from flask import Flask, request
 from markupsafe import escape
+from sensible.loginit import logger
 
+log = logger(__name__)
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+# if __name__ == '__main__':
+#     log.info("START Flask")
+#     app.debug = True
+#     app.run(host='0.0.0.0')
+#     log.info("SHUTDOWN Flask")
